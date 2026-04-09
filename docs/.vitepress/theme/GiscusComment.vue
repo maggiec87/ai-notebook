a6f1389b6166e31e7eb96319af01321bc8fb84ff
maggiec87 <maggiec_87@outlook.com>
2026-04-09 11:30:01 +0800

    Create GiscusComment.vue

---
+++ docs/.vitepress/theme/GiscusComment.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="margin-top: 2rem; padding-top: 1rem; border-top: 1px solid var(--vp-c-divider)">
+    <Giscus
+      repo="你的用户名/仓库名"
+      repo-id="R_xxxxxxxxxx"
+      category="Announcements"
+      category-id="DIC_xxxxxxxxxx"
+      mapping="pathname"
+      reactions-enabled="1"
+      emit-metadata="0"
+      input-position="top"
+      theme="preferred_color_scheme"
+      lang="zh-CN"
+      loading="lazy"
+    />
+  </div>
+</template>
+
+<script setup>
+import Giscus from '@giscus/vue'
+</script>
